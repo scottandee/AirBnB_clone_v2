60b7536527d4e203ba3f9f5d457f314d9b657f85
Andee <andeeolayinkascott@gmail.com>
2023-06-21 14:01:46 +0000

    Add check in each class for filestorage or database

--- models/city.py
+++ models/city.py
@@ -2,6 +2,7 @@
 """ City Module for HBNB project """
 
 
+from os import getenv
 from models.base_model import BaseModel, Base
 from models.place import Place
 from sqlalchemy import Column, String, ForeignKey
@@ -12,8 +13,11 @@ class City(BaseModel, Base):
     '''the city class, contains state ID and name'''
 
     __tablename__ = 'cities'
-
-    name = Column(String(128), nullable=False)
-    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-    places = relationship('Place', cascade='all, delete', backref='cities',
-                          passive_deletes=True)
+    if getenv('HBNB_TYPE_STORAGE') is not None:
+        name = Column(String(128), nullable=False)
+        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+        places = relationship('Place', cascade='all, delete', backref='cities',
+                              passive_deletes=True)
+    else:
+        state_id = ""
+        name = ""
