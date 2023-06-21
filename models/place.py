996b025b0ac895aab196efdb8dbe86dfec44362c
Jany Muong <me.roumuong@gmail.com>
2023-06-21 22:42:54 +0300

    ORM models

--- models/place.py
+++ models/place.py
@@ -4,13 +4,12 @@
 from os import getenv
 from models.base_model import BaseModel, Base
 from models.review import Review
-from sqlalchemy import Column, String, Integer, Float, ForeignKey
+from sqlalchemy import Column, String, Integer, Float, ForeignKey, Table
 from sqlalchemy.orm import relationship
 
 
 class Place(BaseModel, Base):
     """A place to stay"""
-
     __tablename__ = 'places'
 
     if getenv('HBNB_TYPE_STORAGE') is not None:
@@ -26,6 +25,17 @@ class Place(BaseModel, Base):
         longitude = Column(Float, nullable=True)
         reviews = relationship('Review', cascade='all, delete',
                                backref='place', passive_deletes=True)
+
+        if getenv('HBNB_TYPE_STORAGE') == 'db':
+            place_amenity = Table(
+                'place_amenity', Base.metadata,
+                Column('place_id', String(60), ForeignKey('places.id'),
+                       primary_key=True, nullable=False),
+                Column('amenity_id', String(60), ForeignKey('amenities.id'),
+                       primary_key=True, nullable=False)
+            )
+            amenities = relationship('Amenity', secondary=place_amenity,
+                                     viewonly=False)
     else:
         city_id = ""
         user_id = ""
